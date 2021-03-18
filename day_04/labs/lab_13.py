c6f445c51ee9a94fb4bb24355425b0c2529d613d
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2021-03-18 15:20:16 +0530

    Add files via upload

---
+++ day_04/labs/lab_13.py
@@ -0,0 +1,25 @@
+def average(*many):
+    return sum(many)/len(many)
+
+import unittest
+
+if __name__ == "__main__":
+
+    class TestSum(unittest.TestCase):
+
+        def test0(self):
+            pass
+
+        def test1(self):
+            pass
+
+        def test2(self):
+            pass
+
+        def test_random_n(self):
+            pass
+
+        def test_negative(self):
+            pass
+    
+    unittest.main()
